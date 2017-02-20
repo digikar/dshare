0cd08d545f0befea8f62ae038abfe4c3ffe33ae4
Darshan Digikar <darshandigikar1995@gmail.com>
2017-02-20 11:03:54 +0530

    few changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { ClarityModule } from 'clarity-angular';
+//import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
 import { SubmitArticleComponent } from './submit-article/submit-article.component';
 import { NgSemanticModule } from "ng-semantic";
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubmitArticleComponent
+    SubmitArticleComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ClarityModule.forRoot(),
+    //ClarityModule.forRoot(),
   
   ],
   providers: [],
